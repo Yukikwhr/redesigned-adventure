e136b5403a40be65f1eb976838e781c831299f56
Yukikwhr <bokuwatorikawahara@yahoo.co.jp>
2018-12-12 00:03:20 +0900

    develop queue

--- queue.c
+++ queue.c
@@ -4,12 +4,10 @@
 
 #include <pthread.h>
 
-#define PUT_NUM 30 /* 追加するエントリ数 */
-#define GET_NUM 10 /* 取り出すエントリ数 */
-
 struct entry
 {
     struct entry *next;
+    void *(*func)(void *arg);
     void *data;
 };
 
@@ -33,7 +31,7 @@ struct list *list_init(void)
     return (list);
 }
 
-int list_enqueue(struct list *list, void *data)
+int list_enqueue(struct list *list, void *data, void *(*func)(void *arg))
 {
     struct entry *e;
 
@@ -42,6 +40,7 @@ int list_enqueue(struct list *list, void *data)
         return (1);
     e->next = NULL;
     e->data = data;
+    e->func = func;
     pthread_mutex_lock(&list->data_lock);
     *list->tail = e;
     list->tail = &e->next;
@@ -68,78 +67,10 @@ struct entry *list_dequeue(struct list *list)
     return (e);
 }
 
-struct entry *list_traverse(struct list *list, int (*func)(void *, void *), void *user)
+void *test(void *arg)
 {
-    struct entry **prev, *n, *next;
-
-    if (list == NULL)
-        return (NULL);
-
-    pthread_mutex_lock(&list->data_lock);
-    prev = &list->head;
-    for (n = list->head; n != NULL; n = next)
-    {
-        next = n->next;
-        switch (func(n->data, user))
-        {
-        case 0:
-            /* continues */
-            prev = &n->next;
-            break;
-        case 1:
-            /* delete the entry */
-            *prev = next;
-            if (next == NULL)
-                list->tail = prev;
-            pthread_mutex_unlock(&list->data_lock);
-            return (n);
-        case -1:
-        default:
-            /* traversal stops */
-            pthread_mutex_unlock(&list->data_lock);
-            return (NULL);
-        }
-    }
-    pthread_mutex_unlock(&list->data_lock);
-    return (NULL);
-}
-
-int print_entry(void *e, void *u)
-{
-    printf("%s\n", (char *)e);
-    return (0);
-}
-
-int delete_entry(void *e, void *u)
-{
-    char *c1 = e, *c2 = u;
-
-    return (!strcmp(c1, c2));
-}
-
-void *put(void *arg)
-{
-    struct list *list = (struct list *)arg;
-
-    /* 追加する */
-    for (int i = 0; i < PUT_NUM; i++)
-    {
-        list_enqueue(list, strdup("data"));
-    }
-}
-
-void *get(void *arg)
-{
-    struct list *list = (struct list *)arg;
-    struct entry *entry;
-    /* 取り出す */
-    for (int i = 0; i < GET_NUM; i++)
-    {
-        entry = list_dequeue(list);
-        printf("%s\n", (char *)entry->data);
-        free(entry->data);
-        free(entry);
-    }
+    int *value = (int *)arg;
+    printf("%d\n", *(value));
 }
 
 int main()
@@ -149,50 +80,16 @@ int main()
 
     list = list_init();
 
-    /* enqueue data */
-    list_enqueue(list, strdup("first"));
-    list_enqueue(list, strdup("second"));
-    list_enqueue(list, strdup("third"));
+    int value = 3;
+    test((void *)&value);
 
-    /* entry list */
-    list_traverse(list, print_entry, NULL);
-
-    /* delete "second" entry */
-    entry = list_traverse(list, delete_entry, "second");
-    if (entry != NULL)
-    {
-        free(entry->data);
-        free(entry);
-    }
+    list_enqueue(list, (void *)&value, test);
 
     /* dequeue data */
     while ((entry = list_dequeue(list)) != NULL)
     {
-        printf("%s\n", (char *)entry->data);
-        free(entry->data);
+        entry->func(entry->data);
         free(entry);
-    }
-
-    pthread_t putter[2], getter[6];
-
-    for (int i = 0; i < 2; i++)
-    {
-        pthread_create(&putter[i], NULL, put, (void *)list);
-    }
-
-    for (int i = 0; i < 6; i++)
-    {
-        pthread_create(&getter[i], NULL, get, (void *)list);
-    }
-
-    for (int i = 0; i < 2; i++)
-    {
-        pthread_join(putter[i], NULL);
-    }
-
-    for (int i = 0; i < 6; i++)
-    {
-        pthread_join(getter[i], NULL);
     }
 
     free(list);
