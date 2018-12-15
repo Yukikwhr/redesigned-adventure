b065d83461a4003dafce16dbe9c96ae1ce7134c2
Yukikwhr <bokuwatorikawahara@yahoo.co.jp>
2018-12-16 00:31:31 +0900

    complete multithread server

--- server.c
+++ server.c
@@ -22,6 +22,7 @@
 #define LISTEN_BACKLOG 5
 #endif
 
+// キュー
 struct entry
 {
     struct entry *next;
@@ -32,6 +33,7 @@ struct entry
 struct list
 {
     pthread_mutex_t data_lock;
+    pthread_cond_t notempty;
     struct entry *head;
     struct entry **tail;
 };
@@ -46,6 +48,7 @@ struct list *list_init(void)
     list->head = NULL;
     list->tail = &list->head;
     pthread_mutex_init(&list->data_lock, NULL);
+    pthread_cond_init(&list->notempty, NULL);
     return (list);
 }
 
@@ -62,6 +65,7 @@ int list_enqueue(struct list *list, void *data, void *(*func)(void *arg))
     pthread_mutex_lock(&list->data_lock);
     *list->tail = e;
     list->tail = &e->next;
+    pthread_cond_signal(&list->notempty);
     pthread_mutex_unlock(&list->data_lock);
     return (0);
 }
@@ -71,6 +75,10 @@ struct entry *list_dequeue(struct list *list)
     struct entry *e;
 
     pthread_mutex_lock(&list->data_lock);
+    while (list->tail == &list->head)
+    {
+        pthread_cond_wait(&list->notempty, &list->data_lock);
+    }
     if (list->head == NULL)
     {
         pthread_mutex_unlock(&list->data_lock);
@@ -79,18 +87,14 @@ struct entry *list_dequeue(struct list *list)
     e = list->head;
     list->head = e->next;
     if (list->head == NULL)
+    {
         list->tail = &list->head;
-    list->tail = &list->head;
+    }
     pthread_mutex_unlock(&list->data_lock);
     return (e);
 }
 
-void *test(void *arg)
-{
-    int *value = (int *)arg;
-    printf("%d\n", *(value));
-}
-
+// サーバー
 char *program_name = "server";
 
 int open_accepting_socket(int port)
@@ -118,46 +122,48 @@ int open_accepting_socket(int port)
     return (sock);
 }
 
-void protocol_main(int sock)
+/* キューに貯める関数ポインタ */
+void *protocol_main(void *arg)
 {
+    int *sock = (int *)arg;
+    logutil_info("connected");
     char c[1];
-
-    while (read(sock, c, sizeof c) > 0)
+    while (read(*sock, c, sizeof c) > 0)
         /* ignore protocol process */;
     logutil_info("disconnected");
 }
 
-void main_loop(int accepting_socket)
+/* ワーカー*/
+void *worker(void *arg)
 {
-
-    int sock;
-
-    struct sockaddr_in client_addr;
-
-    socklen_t client_addr_size;
+    struct list *list = (struct list *)arg;
+    struct entry *entry;
 
     for (;;)
     {
+        entry = list_dequeue(list);
+        (*entry->func)(entry->data);
+    }
+}
 
+/* リクエスト取得，エンキュー */
+void main_loop(int accepting_socket, struct list *list)
+{
+    int sock;
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size;
+    for (;;)
+    {
         client_addr_size = sizeof(client_addr);
-
         sock = accept(accepting_socket, (struct sockaddr *)&client_addr, &client_addr_size);
-
-        //pthread_t pm;
-
         if (sock < 0)
         {
-
             logutil_error("accept error : % d", errno);
         }
         else
         {
-            protocol_main(sock);
-            /* 接続されたらスレッドを生成し，処理する */
-            //pthread_create(&pm, NULL, (void *)protocol_main, (void *)&sock);
+            list_enqueue(list, (void *)&sock, protocol_main);
         }
-
-        //pthread_join(pm, NULL);
     }
 }
 
@@ -185,7 +191,7 @@ void *handle_sig(void *arg)
     return (NULL);
 }
 
-/*int main(int argc, char **argv)
+int main(int argc, char **argv)
 {
     char *port_number = NULL;
     int ch, sock, server_port = DEFAULT_SERVER_PORT;
@@ -210,52 +216,43 @@ void *handle_sig(void *arg)
     argv += optind;
 
     if (port_number != NULL)
-        server_port = strtol(port_number, NULL, 0);*/
+        server_port = strtol(port_number, NULL, 0);
 
-/* server_portでlistenし，socket descriptorをsockに代入 */
-/*sock = open_accepting_socket(server_port);
+    /* server_portでlistenし，socket descriptorをsockに代入 */
+    sock = open_accepting_socket(server_port);
 
     if (!debug_mode)
     {
         logutil_syslog_open(program_name, LOG_PID, LOG_LOCAL0);
         daemon(0, 0);
-    }*/
-
-/* SIGINTとSIGTERMをブロック */
-/*sigemptyset(&sig);
-    sigaddset(&sig, SIGINT);
-    sigaddset(&sig, SIGTERM);
-    pthread_sigmask(SIG_BLOCK, &sig, NULL);*/
-
-/* スレッド生成 */
-/*pthread_t sig_p;
-    pthread_create(&sig_p, NULL, handle_sig, NULL);
-
-    main_loop(sock);*/
-
-/*NOTREACHED*/
-//return (0);
-//}
-
-int main()
-{
-    struct list *list;
-    struct entry *entry;
-
-    list = list_init();
-
-    int value = 3;
-    test((void *)&value);
-
-    list_enqueue(list, (void *)&value, test);
-
-    /* dequeue data */
-    while ((entry = list_dequeue(list)) != NULL)
-    {
-        entry->func(entry->data);
-        free(entry);
     }
 
-    free(list);
+    /* SIGINTとSIGTERMをブロック */
+    sigemptyset(&sig);
+    sigaddset(&sig, SIGINT);
+    sigaddset(&sig, SIGTERM);
+    pthread_sigmask(SIG_BLOCK, &sig, NULL);
+
+    /* シグナル制御用スレッド生成 */
+    pthread_t sig_p;
+    pthread_create(&sig_p, NULL, handle_sig, NULL);
+
+    struct list *list = list_init();
+
+    /* ワーカスレッド生成 */
+    pthread_t work[THREAD_POOL_NUM];
+    for (int i = 0; i < THREAD_POOL_NUM; i++)
+    {
+        pthread_create(&work[i], NULL, worker, list);
+    }
+
+    main_loop(sock, list);
+
+    for (int i = 0; i < THREAD_POOL_NUM; i++)
+    {
+        pthread_join(work[i], NULL);
+    }
+
+    /*NOTREACHED*/
     return (0);
 }
